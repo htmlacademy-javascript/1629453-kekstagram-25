eb53ccfa38d73d7b87aab3f9e965b78b64c7b548
Keks <keks@htmlacademy.ru>
2022-03-13 08:54:13 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,11 @@
+const getRandomPositiveInteger = (x, y) => {
+  const lower = Math.ceil(Math.min(Math.abs(x), Math.abs(y)));
+  const upper = Math.floor(Math.max(Math.abs(x), Math.abs(y)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const checkStringLength = (string, length) => string.length <= length;
+checkStringLength('Hello, world!', 200);
+
+export {getRandomPositiveInteger};
