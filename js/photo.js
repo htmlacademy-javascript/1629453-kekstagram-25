7fc7c9c93f170f207737197b7eebf7f763abed8a
Иванов Павел <skyteno69@mail.ru>
2022-03-28 17:51:54 +0300

    Отрисуй меня полностью (часть 1) 2.0

--- js/photo.js
+++ js/photo.js
@@ -9,10 +9,11 @@ const fragment = document.createDocumentFragment();
 
 for (let i = 0; i < dataList.length; i++) {
   const pictureClone = template.cloneNode(true);
-  pictureClone.children[0].src = dataList[i].url;
-  pictureClone.children[1].children[0].textContent = dataList[i].likes;
-  pictureClone.children[1].children[1].textContent = dataList[i].comments.length;
+  pictureClone.querySelector('.picture__img').src = dataList[i].url;
+  pictureClone.querySelector('.picture__likes').textContent = dataList[i].likes;
+  pictureClone.querySelector('.picture__comments').textContent = dataList[i].comments.length;
   fragment.append(pictureClone);
 }
 
 pictureContainer.append(fragment);
+
